7d614c49e4663aba10c0531c564eaca502d1d381
danielwanelle <danielwanelle@gmail.com>
2021-04-21 00:30:16 -0300

    Implementado o ORM, Rotas e migrations

--- src/server.ts
+++ src/server.ts
@@ -1,28 +1,11 @@
 import express from "express"; //sintaxe nova, substitui o const require
+import "./database";
+import { routes } from "../routes";
 
 const app = express();
-app.use(
-    express.urlencoded({
-        extended: true
-    })
-)
 
-/**
- * Rotas
- */
-app.get("/", (request, response) => {
-    return response.json({
-        message: "Olá mundo!"
-    });
-});
-
-app.post("/user", (request, response) => {
-    const name = request.body.name;
-    console.log(name);
-    return response.json({
-        message: "Usuário " + name + " cadastrado!"
-    });
-});
+app.use(express.json());
+app.use(routes);
 
 app.listen(3333, () => {
     console.log("Server is running on port 3333");
